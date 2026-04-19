6e85094163ca342666f6ee7e04568c7e07b668ad
William Marques <william.marques@elogroup.com.br>
2026-04-19 15:26:15 -0300

    cancellation and retry

--- web/src/components/upload-widget-upload-item.tsx
+++ web/src/components/upload-widget-upload-item.tsx
@@ -4,16 +4,21 @@ import { Download, ImageUp, Link2, RefreshCcw, X } from "lucide-react";
 import { Button } from "./ui/button";
 import { motion } from "motion/react";
 
-import { type Upload } from "../store/uploads";
+import { type Upload, useUploads } from "../store/uploads";
 import { formatBytes } from "../utils/format-bytes";
 
 interface UploadWidgetUploadItemProps {
   upload: Upload;
+  uploadId: string;
+
 }
 
 export function UploadWidgetUploadItem({
   upload,
+  uploadId,
 }: UploadWidgetUploadItemProps) {
+  const cancelUpload = useUploads((store) => store.cancelUpload);
+
   return (
     <motion.div
       className="p-3 rounded-lg flex flex-col gap-3 shadow-shape-content bg-white/2 relative overflow-hidden"
@@ -37,34 +42,51 @@ export function UploadWidgetUploadItem({
             <span className="text-green-400 ml-1">-94%</span>
           </span>
           <div className="size-1 rounded-full bg-zinc-700" />
-          <span>45%</span>
+          {upload.status === "success" && <span>100%</span>}
+          {upload.status === "progress" && <span>45%</span>}
+          {upload.status === "error" && (
+            <span className="text-red-400">Error</span>
+          )}
+          {upload.status === "canceled" && (
+            <span className="text-yellow-400">Canceled</span>
+          )}
         </span>
       </div>
 
-      <Progress.Root className="bg-zinc-800 rounded-full h-1 overflow-hidden">
+      <Progress.Root
+        data-status={upload.status}
+        className="bg-zinc-800 rounded-full h-1 overflow-hidden group"
+      >
         <Progress.Indicator
-          className="bg-indigo-500 h-1"
-          style={{ width: "43%" }}
+          className="bg-indigo-500 h-1 group-data-[status=success]:bg-green-400 group-data-[status=error]:bg-red-400 group-data-[status=canceled]:bg-yellow-400"
+          style={{ width: upload.status === "progress" ? "45%" : "100%" }}
         />
       </Progress.Root>
 
       <div className="absolute top-2.5 right-2.5 flex items-center gap-1">
-        <Button size="icon-sm">
+         <Button size="icon-sm" disabled={upload.status !== "success"}>
           <Download className="size-4" strokeWidth={1.5} />
           <span className="sr-only">Download compressed image</span>
         </Button>
 
-        <Button size="icon-sm">
+        <Button size="icon-sm" disabled={upload.status !== "success"}>
           <Link2 className="size-4" strokeWidth={1.5} />
           <span className="sr-only">Copy remote URL</span>
         </Button>
 
-        <Button size="icon-sm">
+        <Button
+          disabled={!["canceled", "error"].includes(upload.status)}
+          size="icon-sm"
+        >
           <RefreshCcw className="size-4" strokeWidth={1.5} />
           <span className="sr-only">Retry upload</span>
         </Button>
 
-        <Button size="icon-sm">
+        <Button
+          disabled={upload.status !== "progress"}
+          size="icon-sm"
+          onClick={() => cancelUpload(uploadId)}
+        >
           <X className="size-4" strokeWidth={1.5} />
           <span className="sr-only">Cancel upload</span>
         </Button>
--- web/src/store/uploads.ts
+++ web/src/store/uploads.ts
@@ -1,39 +1,95 @@
 import { create } from "zustand";
 import { enableMapSet } from "immer";
 import { immer } from "zustand/middleware/immer";
+import { uploadFileToStorage } from "../http/upload-file-to-storage";
 
 export type Upload = {
   name: string;
   file: File;
+  abortController: AbortController;
+  status: "progress" | "success" | "error" | "canceled";
 };
 
 export type UploadState = {
   uploads: Map<string, Upload>;
   addUploads: (files: File[]) => void;
+  cancelUpload: (uploadId: string) => void;
 };
 
 enableMapSet();
 
 export const useUploads = create<UploadState>()(
-  immer((set) => {
+  immer((set, get) => {
+    async function processUpload(uploadId: string) {
+      const upload = get().uploads.get(uploadId);
+
+      if (!upload) {
+        return;
+      }
+
+      try {
+        await uploadFileToStorage(
+          { file: upload.file },
+          { signal: upload.abortController.signal }
+        );
+
+        set((state) => {
+          state.uploads.set(uploadId, {
+            ...upload,
+            status: "success",
+          });
+        });
+      } catch (error) {
+        set((state) => {
+          state.uploads.set(uploadId, {
+            ...upload,
+            status: "error",
+          });
+        });
+      }
+    }
+
+    function cancelUpload(uploadId: string) {
+      const upload = get().uploads.get(uploadId);
+
+      if (!upload) {
+        return;
+      }
+
+      upload.abortController.abort();
+
+      set((state) => {
+        state.uploads.set(uploadId, {
+          ...upload,
+          status: "canceled",
+        });
+      });
+    }
+
     function addUploads(files: File[]) {
       for (const file of files) {
         const uploadId = crypto.randomUUID();
+        const abortController = new AbortController();
 
         const upload: Upload = {
           name: file.name,
           file,
+          abortController,
+          status: "progress",
         };
 
         set((state) => {
           state.uploads.set(uploadId, upload);
         });
+
+        processUpload(uploadId);
       }
     }
 
     return {
-      uploads: new Map<string, Upload>(),
+      uploads: new Map(),
       addUploads,
+      cancelUpload,
     };
   })
 );
